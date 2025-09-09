162281e2f3c7ab8e129b300402e4cd8547201aaa
Hiago-Santana <hiago_douglas@hotmail.com>
2025-09-09 17:20:01 -0300

    update

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex border-b-2 w-full">
+        <div class="flex gap-2">
+            <div class="flex justify-center items-center text-white text-2xl bg-gray-900 rounded-md p-2 font-bold">R$</div>
+            <h1 class="font-bold">
+                FinanceControl
+            </h1>
+        </div>
+    </div>
+</template>
