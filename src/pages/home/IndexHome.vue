d326d08f9414e8ce266dfe84cf1bacc7f5b1a943
Hiago-Santana <hiago_douglas@hotmail.com>
2025-09-17 17:39:24 -0300

    update

--- src/pages/home/IndexHome.vue
+++ src/pages/home/IndexHome.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
-        <transaction-summary></transaction-summary>
+        <transaction-summary :transactions="transactions"></transaction-summary>
     </div>
 </template>
 <script setup>
+import { ref, onMounted } from 'vue';
+import { openDb, listTransactions } from '../../composables/useIndexedDB';
 import TransactionSummary from './components/TransactionSummary.vue';
+
+const database = ref();
+const transactions = ref();
+
+onMounted(async () => {
+    database.value = await openDb();
+    transactions.value = await listTransactions(database.value);
+})
 
 </script>
--- src/pages/home/components/TransactionSummary.vue
+++ src/pages/home/components/TransactionSummary.vue
@@ -1,9 +1,24 @@
 <template>
     <h1>TransactionSummary</h1>
+    {{ transactions }}
+    <div class="flex flex-col">
+        <span>Saldo Total</span>
+        <span></span>
+
+    </div>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, toRef, onMounted, computed } from 'vue';
 import { openDb, listTransactions } from '../../../composables/useIndexedDB';
 
+const props = defineProps([ "transactions" ])
+const transactionsProp = toRef(props, 'transactions')
+
+const sum = computed(() => { return transactionsProp.value.reduce((sum, item) => sum + item.value, 0)});
+
+const database = ref();
+onMounted(async () => {
+    console.log("props.transactions",sum)
+})
 
 </script>
